32ea1017b4e10ac46e6d75861fe802fa22a04770
peterbaile <peterchenwflms@gmail.com>
2022-10-25 16:10:09 -0400

    retrieve all tables from query

--- generate_tables.py
+++ generate_tables.py
@@ -1,4 +1,7 @@
+# each table label is an array of tables
+
 import json
+from sql_metadata import Parser
 
 with open('./spider_data/tables.json') as f:
   tables_data = json.load(f)
@@ -17,27 +20,22 @@ dev_data_new = []
 
 count = 0
 for q in dev_data:
-  q_sql = q['query_toks']
-  q_sql = [word.lower() for word in q_sql]
-
-  # skip join for now
-  if 'join' in q_sql:
-    continue
-
-  # search for the word that comes right after FROM
-  # print(q_sql)
-  q_table = q_sql[q_sql.index('from') + 1].lower()
+  q_sql = q['query']
+  
+  q_tables = Parser(q_sql).tables
+  q_tables = [table.lower() for table in q_tables]
 
   # sanity check that this table is actually in table.json
-  assert(q_table in tables)
+  for table in q_tables:
+    assert(table in tables)
 
-  q['table_name'] = q_table
+  q['table_name'] = q_tables
 
   dev_data_new.append(q)
 
   count += 1
 
-with open('./dev_new.json', 'w') as f:
+with open('./spider_data/dev_new.json', 'w') as f:
   json.dump(dev_data_new, f, indent=4)
 
 print(f'{count} / {len(dev_data)} queries')
