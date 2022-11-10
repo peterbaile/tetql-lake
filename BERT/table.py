c1a683912c1d4a707bb998fa6a5f57e332cf17be
peterbaile <peterchenwflms@gmail.com>
2022-11-10 09:18:45 -0500

    test size = 0.3

--- BERT/table.py
+++ BERT/table.py
@@ -30,7 +30,7 @@ def tokenize(texts):
 # print(tokenizer.convert_ids_to_tokens(tokenized[0].input_ids[0]))
 
 df = pd.read_csv('./data.csv')
-train, test = train_test_split(df, test_size = 0.01, random_state = 123, shuffle = True) # TODO: change test_size back to 0.3
+train, test = train_test_split(df, test_size = 0.3, random_state = 123, shuffle = True)
 train_X, train_Y = train.iloc[:, 0], train.iloc[:, 1]
 test_X, test_Y = test.iloc[:, 0], test.iloc[:, 1]
 
