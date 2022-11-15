3b3a65173ac379674dfaa457cc65313473e4cf7a
peterbaile <peterchenwflms@gmail.com>
2022-11-15 08:51:11 -0500

    train new

--- BERT/table.py
+++ BERT/table.py
@@ -29,13 +29,12 @@ def tokenize(texts):
 # print(tokenize(['[COL]'])[0].input_ids)
 # print(tokenizer.convert_ids_to_tokens(tokenized[0].input_ids[0]))
 
-df = pd.read_csv('./data_join_no_special.csv')
-train, test = train_test_split(df, test_size = 0.3, random_state = 123, shuffle = True)
-train_X, train_Y = train.iloc[:, 0], train.iloc[:, 1]
-test_X, test_Y = test.iloc[:, 0], test.iloc[:, 1]
-
-# test = pd.read_csv('./data_join_only_1.csv')
+df = pd.read_csv('./data/train_others/all.csv')
+# train, test = train_test_split(df, test_size = 0.3, random_state = 123, shuffle = True)
+# train_X, train_Y = train.iloc[:, 0], train.iloc[:, 1]
 # test_X, test_Y = test.iloc[:, 0], test.iloc[:, 1]
+
+test_X, test_Y = df.iloc[:, 0], df.iloc[:, 1]
 
 # model = BertModel.from_pretrained('bert-base-uncased')
 
