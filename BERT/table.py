dd152e68e170eb0554507bcc770e514bace3b72e
peterbaile <peterchenwflms@gmail.com>
2022-11-15 23:22:45 -0500

    train v3 diff 7

--- BERT/table.py
+++ BERT/table.py
@@ -29,7 +29,7 @@ def tokenize(texts):
 # print(tokenize(['[COL]'])[0].input_ids)
 # print(tokenizer.convert_ids_to_tokens(tokenized[0].input_ids[0]))
 
-df = pd.read_csv('./data/train_spider/no_join_v3.csv')
+df = pd.read_csv('./data/train_spider/no_join_v3_diff_7.csv')
 train, test = train_test_split(df, test_size = 0.3, random_state = 123, shuffle = True) # TODO: change test size = 0.3
 train_X, train_Y = train.iloc[:, 0], train.iloc[:, 1]
 test_X, test_Y = test.iloc[:, 0], test.iloc[:, 1]
@@ -108,7 +108,7 @@ if __name__ == '__main__':
       
       print(f'\nTrain accuracy: {total_acc_train} / {train_X.shape[0]}')
 
-    torch.save(model, './bert_no_join_v3.pt')
+    torch.save(model, './bert_no_join_v3_diff_7.pt')
   elif args.mode == 'test':
     model = torch.load('./bert.pt')
     test_dataset = LogDataset(test_X, test_Y)
