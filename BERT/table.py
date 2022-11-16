f86fecc7f3097c81e6be9f9616ac4683fc692a2b
peterbaile <peterchenwflms@gmail.com>
2022-11-15 22:01:03 -0500

    train no join

--- BERT/table.py
+++ BERT/table.py
@@ -29,8 +29,8 @@ def tokenize(texts):
 # print(tokenize(['[COL]'])[0].input_ids)
 # print(tokenizer.convert_ids_to_tokens(tokenized[0].input_ids[0]))
 
-df = pd.read_csv('./data/dev/data_join_no_special.csv')
-train, test = train_test_split(df, test_size = 0.6, random_state = 123, shuffle = True) # TODO: change test size = 0.3
+df = pd.read_csv('./data/train_spider/no_join.csv')
+train, test = train_test_split(df, test_size = 0.3, random_state = 123, shuffle = True) # TODO: change test size = 0.3
 train_X, train_Y = train.iloc[:, 0], train.iloc[:, 1]
 test_X, test_Y = test.iloc[:, 0], test.iloc[:, 1]
 
@@ -67,7 +67,7 @@ class BertClassifier(nn.Module):
 
 device = 'cuda'
 
-max_epochs = 3
+max_epochs = 5
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
