44868b6a9d75f65017fbe38e7d11e8c991f38e10
peterbaile <peterchenwflms@gmail.com>
2022-11-18 09:24:51 -0500

    train final hope

--- BERT/table.py
+++ BERT/table.py
@@ -3,7 +3,7 @@
 # regression with dropout + linear layer with 1 output
 
 from sklearn.model_selection import train_test_split
-from transformers import BertTokenizer, BertModel
+from transformers import BertTokenizer, BertModel, AutoModel, AutoTokenizer
 from torch.utils import data
 import torch
 from torch import nn
@@ -19,12 +19,14 @@ random.seed(0)
 
 # tokenizer.add_special_tokens({'additional_special_tokens': ['[TBL]', '[COL]']})
 
-BERT_MODEL_TYPE = 'bert-base-uncased'
+BERT_MODEL_TYPE = 'prajjwal1/bert-tiny'
 
-tokenizer = BertTokenizer.from_pretrained(BERT_MODEL_TYPE)
+# tokenizer = BertTokenizer.from_pretrained(BERT_MODEL_TYPE)
+
+tokenizer = AutoTokenizer.from_pretrained(BERT_MODEL_TYPE)
 
 def tokenize(texts):
-  return [tokenizer(text, padding='max_length', return_tensors='pt') for text in tqdm(texts)]
+  return [tokenizer(text, padding='max_length', max_length=512, return_tensors='pt') for text in tqdm(texts)]
 
 # tokenized = tokenize(['How many singers do we have? [SEP] [TBL] perpetrator [COL] perpetrator id [COL] people id [COL] date [COL] year [COL] location [COL] country [COL] killed [COL] injured'])
 # print(tokenized[0].input_ids)
@@ -46,10 +48,11 @@ class BertClassifier(nn.Module):
   def __init__(self, dropout=0.5):
     super(BertClassifier, self).__init__()
 
-    self.bert = BertModel.from_pretrained(BERT_MODEL_TYPE)
+    # self.bert = BertModel.from_pretrained(BERT_MODEL_TYPE)
+    self.bert = AutoModel.from_pretrained(BERT_MODEL_TYPE)
     self.bert.resize_token_embeddings(len(tokenizer))
     self.dropout = nn.Dropout(dropout)
-    self.linear = nn.Linear(768, 2)
+    self.linear = nn.Linear(128, 2)
 
   def forward(self, input_id, mask):
     _, pooled_output = self.bert(input_ids=input_id, attention_mask=mask, return_dict=False)
@@ -66,32 +69,46 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
 
   parser.add_argument('mode', type=str)
-  parser.add_argument('file', type=str)
+  parser.add_argument('path', type=str)
 
   args = parser.parse_args()
 
   print(args.mode)
-  print(f'source file: {args.file}')  
-
-  df = pd.read_csv(f'./data/{args.file}')
+  print(f'source path: {args.path}')  
 
   if args.mode == 'train':
-    train, test = train_test_split(df, test_size = 0.3, random_state = 123, shuffle = True)
-    train_X, train_Y = train.iloc[:, 0], train.iloc[:, 1]
-    test_X, test_Y = test.iloc[:, 0], test.iloc[:, 1]
+    train_df = pd.read_csv(f'./data/{args.path}/train.csv')
+    valid_df = pd.read_csv(f'./data/{args.path}/valid.csv')
+    train_X, train_Y = train_df.iloc[:, 0], train_df.iloc[:, 1]
+    valid_X, valid_Y = valid_df.iloc[:, 0], valid_df.iloc[:, 1]
 
-    train_batch_size = 40
+    train_batch_size = 2
     model = BertClassifier().to(device)
     print('finished downloading')
-    dataset = LogDataset(train_X, train_Y)
-    dataloader = data.DataLoader(dataset, batch_size = train_batch_size, shuffle = True)
+    train_dataset = LogDataset(train_X, train_Y)
+    valid_dataset = LogDataset(valid_X, valid_Y)
+
+    train_dataloader = data.DataLoader(train_dataset, batch_size = train_batch_size, shuffle = True)
+    valid_dataloader = data.DataLoader(valid_dataset, batch_size = train_batch_size, shuffle = True)
+
     criterion = nn.CrossEntropyLoss().to(device)
     optimizer = Adam(model.parameters(), lr=1e-5)
 
+    train_losses = []
+    valid_losses = []
+
+    avg_train_losses = []
+    avg_valid_losses = []
+    best_valid_loss = None
+
+    patience_cnt = 0
+    patience = 5
+
     for epoch in range(max_epochs):
       total_acc_train = 0
-
-      for train_input, train_labels in tqdm(dataloader):
+      
+      model.train()
+      for train_input, train_labels in tqdm(train_dataloader):
         train_labels = train_labels.to(device)
         mask = train_input['attention_mask'].to(device)
         input_id = train_input['input_ids'].squeeze(1).to(device)
@@ -106,13 +123,44 @@ if __name__ == '__main__':
         
         loss.backward()
         optimizer.step()
-      
-      with torch.no_grad():
-        valid
-      
-      print(f'\nTrain accuracy: {total_acc_train} / {train_X.shape[0]}')
 
-    torch.save(model, './bert_no_join_v3_diff_7.pt')
+        train_losses.append(loss.item())
+      
+      model.eval()
+      with torch.no_grad():
+        for valid_input, valid_labels in tqdm(valid_dataloader):
+          valid_labels = valid_labels.to(device)
+          mask = valid_input['attention_mask'].to(device)
+          input_id = valid_input['input_ids'].squeeze(1).to(device)
+
+          output = model(input_id, mask)
+
+          loss = criterion(output, valid_labels.long())
+
+          valid_losses.append(loss.item())
+      
+      train_loss = np.average(train_losses)
+      valid_loss = np.average(valid_losses)
+
+      avg_train_losses.append(train_loss)
+      avg_valid_losses.append(valid_loss)
+
+      print(f'\n epoch {epoch}: training loss {train_loss:.5f}, validation loss {valid_loss:.5f}')
+
+      if best_valid_loss is None:
+        torch.save(model, f'./data/{args.path}/bert.pt')
+        best_valid_loss = valid_loss
+      elif valid_loss >= best_valid_loss:
+        patience_cnt += 1
+        print(f'early stopping {patience_cnt}/{patience}')
+
+        if patience_cnt >= patience:
+          print('early stopping')
+          break
+      else:
+        torch.save(model, f'./data/{args.path}/bert.pt')
+        best_valid_loss = valid_loss
+        patience_cnt = 0
 
   elif args.mode == 'dev':
     model = torch.load('./bert.pt')
