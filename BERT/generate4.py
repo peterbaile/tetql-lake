0d483bf71e19ee65476a070aa75dee2ed7774040
peterbaile <peterchenwflms@gmail.com>
2022-11-17 19:09:03 -0500

    update generate4 script

--- BERT/generate4.py
+++ BERT/generate4.py
@@ -1,10 +1,66 @@
-# 1. data leakage: generate train, valid, test, dev such that they all come from different databases
-# split ratio: 60-20-20
+# 1. data leakage: generate train, valid such that they all come from different databases (dev is just dev.json, no test.json)
+# split ratio: 60-40
 # 2-1. constrastive learning: use the top candidates from tf-idf as negative samples
 # 2-2. more positive samples from combinations
 
 import json
 import argparse
+from itertools import combinations
+import pandas as pd
+
+def generate_samples(q_idx_ls, q_data, neg_data):
+  all_data = []
+
+  for q_idx in q_idx_ls:
+    q = q_data[q_idx]
+
+    q_question = q['question']
+    q_db_id = q['db_id']
+    q_table_labels = q['table_labels']
+    q_column_labels = q['column_labels']
+
+    # generate positive samples
+    positive_samples = []
+    positive_cnt = 0
+
+    for q_table_idx in q_table_labels:
+      # fetch all columns that belong to the matched table
+      matched_columns = set()
+      min_columns = set()
+      for idx, column in enumerate(dbs_tables[q_db_id]['column_names']):
+        if column[0] == q_table_idx:
+          matched_columns.add(column[1])
+        
+        if idx != 0 and idx in q_column_labels:
+          min_columns.add(column[1])
+        
+      difference_columns = list(matched_columns.difference(min_columns))
+      difference_columns = difference_columns[:7]
+      min_columns = list(min_columns)
+
+      for l in range(len(difference_columns) + 1):
+        for diff_col in list(combinations(difference_columns, l)):
+          actual_cols = []
+          actual_cols += diff_col
+          actual_cols += min_columns
+
+          text = f"{q_question} [SEP] {table[1]} {' '.join(actual_cols)}"
+          positive_samples.append([text, 1])
+          positive_cnt += 1
+    
+    # generate negative samples (same num as positive samples)
+    negative_samples = []
+    negative_cnt = 0
+
+    for (db_id, tbl_idx) in neg_data[q_idx][:positive_cnt]:
+      cols = ' '.join(dbs[(db_id, tbl_idx)])
+      text = f'{q_question} [SEP] {dbs_table_names[(db_id, tbl_idx)]} {cols}'
+      negative_samples.append([text, 0])
+    
+    positive_samples += negative_samples
+    all_data.append(positive_samples)
+  
+  return all_data
 
 if __name__ == '__main__':
   # parser = argparse.ArgumentParser()
@@ -15,11 +71,17 @@ if __name__ == '__main__':
   with open('../spider_data/train_spider_new.json', 'r') as f:
     q_data = json.load(f)
   
+  with open('../spider_data/trian_spider_idf.json', 'r') as f:
+    neg_data = json.load(f)
+  
   # get count of how many questions per database and sort and split
   db_q_count = {}
   db_q = {}
 
   for idx, q in enumerate(q_data):
+    if len(q['table_labels']) >= 2:
+      continue
+  
     q_db_id = q['db_id']
     if q_db_id not in db_q_count:
       db_q_count[q_db_id] = 1
@@ -30,53 +92,33 @@ if __name__ == '__main__':
 
   db_q_count = dict(sorted(db_q_count.items(), key = lambda x: x[1], reverse = True))
 
-  num_q = len(q_data)
+  num_q = sum(db_q_count.values())
 
   train_len = int(0.6 * num_q)
-  valid_len = int(0.2 * num_q)
-  test_len = num_q - train_len - valid_len
+  valid_len = num_q - train_len
 
-  print(train_len, valid_len, test_len)
+  print(f'train_len, valid_len: {train_len, valid_len}')
 
   train_count = 0
   train_q = []
   valid_count = 0
   valid_q = []
-  test_q = []
   for db_id in db_q_count:
     if train_count < train_len:
       train_count += db_q_count[db_id]
       train_q += db_q[db_id]
-    elif valid_count < valid_len:
+    else:
       valid_count += db_q_count[db_id]
       valid_q += db_q[db_id]
-    else:
-      test_q += db_q[db_id]
   
-  print(len(train_q), len(valid_q), len(test_q))
+  print(len(train_q), len(valid_q))
 
-  
-  # print(db_q_count)
+  train_data = []
 
-  # with open('../spider_data/tables.json') as f:
-  #   db_data = json.load(f)
-  
-  # for db in db_data:
-  # db_id = db['db_id']
-  # num_tables = len(db['table_names'])
+  train_data = generate_samples(train_q, q_data, neg_data)
+  valid_data = generate_samples(valid_q, q_data, neg_data)
 
-  # dbs_table_names[db_id] = db['table_names']
-
-  # dbs_tables[db_id] = db
-
-  # table_cols = {}
-
-  # for i in range(num_tables):
-  #   table_cols[i] = []
-
-  # for col in db['column_names']:
-  #   if col[0] >= 0:
-  #     table_cols[col[0]].append(col[1])
-    
-  # for table_index in table_cols:
-  #   dbs.append((db_id, db['table_names'][table_index], table_cols[table_index]))
+  train_df = pd.DataFrame(train_data, columns=['text', 'label'])
+  valid_df = pd.DataFrame(valid_data, columns=['text', 'label'])
+  train_df.to_csv('./data/train_spider/train.csv', index=False)
+  valid_df.to_csv('./data/train_spider/valid.csv', index=False)
