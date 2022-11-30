29894598454eec79f5e7f25b7e5e245b8012da9a
peterbaile <peterchenwflms@gmail.com>
2022-11-30 08:01:05 -0500

    dev part 0

--- BERT/bert-table.sh
+++ BERT/bert-table.sh
@@ -7,4 +7,5 @@ module load anaconda/2021b
 conda activate
 
 # Run the script
-python table_ranking_join.py --mode train --path train_spider --join True
+# python table_ranking_join.py --mode train --path train_spider --join True
+python table_ranking_join.py --mode dev --path train_spider --devfile dev_join --topk 4 --devpart 0
