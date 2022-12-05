9e70bcb4e0db10f3b1856c3b611550d8ff3ce9cd
peterbaile <peterchenwflms@gmail.com>
2022-12-05 11:47:50 -0500

    rerank dev join 2

--- BERT/bert-table.sh
+++ BERT/bert-table.sh
@@ -8,4 +8,4 @@ conda activate
 
 # Run the script
 # python table_ranking_join.py --mode train --path train_spider --join True
-python table_ranking_join.py --mode dev --path train_spider --devfile dev_join --topk 4 --devpart 2 --join True
+python table_ranking_join.py --mode dev --path train_spider --devfile dev_join_2 --topk 2 --devpart -1 --rerank True
