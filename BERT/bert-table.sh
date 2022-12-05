195981c81596df8a33d517b61dee2180f22d7557
peterbaile <peterchenwflms@gmail.com>
2022-12-05 17:49:48 -0500

    add table ranking 2

--- BERT/bert-table.sh
+++ BERT/bert-table.sh
@@ -7,5 +7,7 @@ module load anaconda/2021b
 conda activate
 
 # Run the script
-python table_ranking.py --mode train --path train_spider
+# python table_ranking.py --mode train --path train_spider
+python table_ranking2.py --mode train --path train_spider
+
 # python table_ranking_join.py --mode dev --path train_spider --devfile dev_join_2 --topk 2 --devpart -1
