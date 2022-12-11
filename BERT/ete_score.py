574503766f951b61675fdb19a5577df01db5e4a2
peterbaile <peterchenwflms@gmail.com>
2022-12-11 15:40:46 -0500

    add pos to rerank

--- BERT/ete_score.py
+++ BERT/ete_score.py
@@ -183,11 +183,11 @@ if __name__ == '__main__':
       #   max_indices_reranked = []
 
       num_tables = 0
-      for max_i in max_indices:
+      for pos, max_i in enumerate(max_indices):
         if args.rerank:
           cand_db_id = dev_df.iloc[i * dev_batch_size + max_i]['db_id']
           if cand_db_id not in db_count:
-            db_count[cand_db_id] = [1, max_i, [max_i]]
+            db_count[cand_db_id] = [1, pos, [max_i]]
           else:
             db_count[cand_db_id][0] += 1
             db_count[cand_db_id][2].append(max_i)
@@ -201,7 +201,7 @@ if __name__ == '__main__':
         # db_id_set.add(dev_df.iloc[i * dev_batch_size + max_i]['db_id'])
       
       if args.rerank:
-        _, _, max_indices_reranked = sorted(db_count.items(), key=lambda item: (item[1][0], item[1][1]), reverse=True)[0][1]
+        _, _, max_indices_reranked = sorted(db_count.items(), key=lambda item: (item[1][0], -item[1][1]), reverse=True)[0][1]
         max_indices_reranked = max_indices_reranked[:args.topk]
 
         for max_i in max_indices_reranked:
