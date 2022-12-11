d173d5e425a13ad3259ccf87bd406bb8d40cb3cc
peterbaile <peterchenwflms@gmail.com>
2022-12-11 09:23:11 -0500

    debug

--- BERT/ete_score.py
+++ BERT/ete_score.py
@@ -180,6 +180,8 @@ if __name__ == '__main__':
       if args.rerank:
         _, max_indices_reranked = sorted(db_count.items(), key=lambda item: item[1][0], reverse=True)[0]
 
+        print(max_indices_reranked)
+
         for max_i in max_indices_reranked:
           output[max_i] = 1
       
