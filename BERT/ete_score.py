76c65d6e11db490bb72ceb6e0beae7a55b2fa452
peterbaile <peterchenwflms@gmail.com>
2022-12-14 08:38:29 +0800

    benchmark

--- BERT/ete_score.py
+++ BERT/ete_score.py
@@ -126,10 +126,6 @@ def evaluate(dev_filename, CANDS_PATH):
       picard_cands_dict[q] = [q_db_id, [q_tbl_idx], gold_sql_dict[q]]
     else:
       picard_cands_dict[q][1].append(q_tbl_idx)
-  
-  # df = pd.DataFrame(all_data, columns=['text', 'db_id', 'table_index', 'label'])
-
-  # df.to_csv(f'./data/dev/{args.filename}_ranking.csv', index=False)
 
   pred_queries, gold_queries = generate_queries(picard_cands_dict)
 
@@ -139,136 +135,133 @@ def evaluate(dev_filename, CANDS_PATH):
   with open(f'./data/eval/{dev_filename}_gold.txt', 'w') as f:
     f.write('\n'.join(gold_queries))
 
-def evaluate_all_tables():
-  return
+# if __name__ == '__main__':
+#   parser = argparse.ArgumentParser()
 
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
+#   parser.add_argument('--path', type=str)
+#   parser.add_argument('--devfile', type=str)
+#   parser.add_argument('--addnegative', type=bool, default=False)
+#   parser.add_argument('--join', type=bool, default=False)
+#   parser.add_argument('--topk', type=int)
+#   parser.add_argument('--rerank', action='store_true')
+#   parser.add_argument('--topnum', type=int, default=-1)
 
-  parser.add_argument('--path', type=str)
-  parser.add_argument('--devfile', type=str)
-  parser.add_argument('--addnegative', type=bool, default=False)
-  parser.add_argument('--join', type=bool, default=False)
-  parser.add_argument('--topk', type=int)
-  parser.add_argument('--rerank', action='store_true')
-  parser.add_argument('--topnum', type=int, default=-1)
+#   args = parser.parse_args()
 
-  args = parser.parse_args()
+#   CANDS_PATH = f'./data/dev/{args.devfile}_ranking_cands.csv'
 
-  CANDS_PATH = f'./data/dev/{args.devfile}_ranking_cands.csv'
+#   if exists(CANDS_PATH):
+#     evaluate(args.devfile, CANDS_PATH)
+#     sys.exit(0)
 
-  if exists(CANDS_PATH):
-    evaluate(args.devfile, CANDS_PATH)
-    sys.exit(0)
+#   MODEL_PATH = suffix(f'./data/{args.path}/{MODEL_TYPE}-ranking', args, '-', '.pt')
+#   print(f'source path: {args.path}, {MODEL_TYPE}, {MODEL_PATH}, add negative: {args.addnegative}')
 
-  MODEL_PATH = suffix(f'./data/{args.path}/{MODEL_TYPE}-ranking', args, '-', '.pt')
-  print(f'source path: {args.path}, {MODEL_TYPE}, {MODEL_PATH}, add negative: {args.addnegative}')
+#   print(f'dev file: {args.devfile}, topk: {args.topk}, re-rank: {args.rerank}, top #cands: {args.topnum}')
+#   model = torch.load(MODEL_PATH)
+#   dev_batch_size = 81 # this has to be the same as the number of candidates picked (81 no join, 100 idf)
 
-  print(f'dev file: {args.devfile}, topk: {args.topk}, re-rank: {args.rerank}, top #cands: {args.topnum}')
-  model = torch.load(MODEL_PATH)
-  dev_batch_size = 81 # this has to be the same as the number of candidates picked (81 no join, 100 idf)
+#   dev_df = pd.read_csv(f'./data/dev/{args.devfile}_ranking.csv')
 
-  dev_df = pd.read_csv(f'./data/dev/{args.devfile}_ranking.csv')
+#   dev_X, dev_Y = dev_df.iloc[:, 0], dev_df.iloc[:, -1]
+#   dev_dataset = DevDataset(dev_X, dev_Y)
+#   dev_dataloader = data.DataLoader(dev_dataset, batch_size = dev_batch_size)
 
-  dev_X, dev_Y = dev_df.iloc[:, 0], dev_df.iloc[:, -1]
-  dev_dataset = DevDataset(dev_X, dev_Y)
-  dev_dataloader = data.DataLoader(dev_dataset, batch_size = dev_batch_size)
+#   total_output = None
+#   total_max_indices = None
 
-  total_output = None
-  total_max_indices = None
+#   model.eval()
+#   with torch.no_grad():
+#     for batch_idx, (test_input, test_label) in enumerate(tqdm(dev_dataloader)):
+#       test_label = test_label.to(device)
+#       mask = test_input['attention_mask'].to(device)
+#       input_id = test_input['input_ids'].squeeze(1).to(device)
 
-  model.eval()
-  with torch.no_grad():
-    for batch_idx, (test_input, test_label) in enumerate(tqdm(dev_dataloader)):
-      test_label = test_label.to(device)
-      mask = test_input['attention_mask'].to(device)
-      input_id = test_input['input_ids'].squeeze(1).to(device)
-
-      raw_output = model(input_id, mask).squeeze(1)
+#       raw_output = model(input_id, mask).squeeze(1)
       
-      if args.rerank:
-        _, max_indices = torch.topk(raw_output, args.topnum)
-      else:
-        _, max_indices = torch.topk(raw_output, args.topk)
+#       if args.rerank:
+#         _, max_indices = torch.topk(raw_output, args.topnum)
+#       else:
+#         _, max_indices = torch.topk(raw_output, args.topk)
 
-      max_indices = max_indices.tolist()
+#       max_indices = max_indices.tolist()
 
-      output = [0 for _ in range(dev_batch_size)]
+#       output = [0 for _ in range(dev_batch_size)]
 
-      # if args.rerank:
-      #   max_idx = torch.argmax(raw_output).item()
-      #   max_db_id = dev_df.iloc[i * dev_batch_size + max_idx]['db_id']
+#       # if args.rerank:
+#       #   max_idx = torch.argmax(raw_output).item()
+#       #   max_db_id = dev_df.iloc[i * dev_batch_size + max_idx]['db_id']
       
-      # db_id_set = set()
+#       # db_id_set = set()
 
-      db_count = {}
+#       db_count = {}
 
-      # if args.rerank:
-      #   max_indices_reranked = []
+#       # if args.rerank:
+#       #   max_indices_reranked = []
 
-      num_tables = 0
-      for pos, max_i in enumerate(max_indices):
-        if args.rerank:
-          cand_db_id = dev_df.iloc[batch_idx * dev_batch_size + max_i]['db_id']
-          if cand_db_id not in db_count:
-            db_count[cand_db_id] = [pos, [max_i]]
-          else:
-            db_count[cand_db_id][1].append(max_i)
-          # if num_tables < args.topk and cand_db_id == max_db_id:
-          #   num_tables += 1
-          #   output[max_i] = 1
-          #   max_indices_reranked.append(max_i)
-        else:
-          output[max_i] = 1
+#       num_tables = 0
+#       for pos, max_i in enumerate(max_indices):
+#         if args.rerank:
+#           cand_db_id = dev_df.iloc[batch_idx * dev_batch_size + max_i]['db_id']
+#           if cand_db_id not in db_count:
+#             db_count[cand_db_id] = [pos, [max_i]]
+#           else:
+#             db_count[cand_db_id][1].append(max_i)
+#           # if num_tables < args.topk and cand_db_id == max_db_id:
+#           #   num_tables += 1
+#           #   output[max_i] = 1
+#           #   max_indices_reranked.append(max_i)
+#         else:
+#           output[max_i] = 1
         
-        # db_id_set.add(dev_df.iloc[i * dev_batch_size + max_i]['db_id'])
+#         # db_id_set.add(dev_df.iloc[i * dev_batch_size + max_i]['db_id'])
       
-      if args.rerank:
-        db_count = sorted(db_count.items(), key=lambda item: item[1][0], reverse=False)
-        i = 0
-        while len(db_count[i][1][1]) < args.topk and i < len(db_count):
-          i += 1
-        _, max_indices_reranked = db_count[i][1]
+#       if args.rerank:
+#         db_count = sorted(db_count.items(), key=lambda item: item[1][0], reverse=False)
+#         i = 0
+#         while len(db_count[i][1][1]) < args.topk and i < len(db_count):
+#           i += 1
+#         _, max_indices_reranked = db_count[i][1]
 
-        # _, _, max_indices_reranked = sorted(db_count.items(), key=lambda item: (item[1][0], -item[1][1]), reverse=True)[0][1]
-        max_indices_reranked = max_indices_reranked[:args.topk]
+#         # _, _, max_indices_reranked = sorted(db_count.items(), key=lambda item: (item[1][0], -item[1][1]), reverse=True)[0][1]
+#         max_indices_reranked = max_indices_reranked[:args.topk]
 
-        for max_i in max_indices_reranked:
-          output[max_i] = 1
+#         for max_i in max_indices_reranked:
+#           output[max_i] = 1
       
-      # assert(len(db_id_set) == 1)
+#       # assert(len(db_id_set) == 1)
 
-      if total_output is None:
-        total_output = output
-        # total_output_prob = (raw_output.max()).cpu()
-      else:
-        total_output += output
-        # total_output_prob = torch.vstack((total_output_prob, (raw_output.max()).cpu()))
+#       if total_output is None:
+#         total_output = output
+#         # total_output_prob = (raw_output.max()).cpu()
+#       else:
+#         total_output += output
+#         # total_output_prob = torch.vstack((total_output_prob, (raw_output.max()).cpu()))
 
-      if args.rerank:
-        max_indices = [x + batch_idx * dev_batch_size for x in max_indices_reranked]
-      else:
-        max_indices = [x + batch_idx * dev_batch_size for x in max_indices]
+#       if args.rerank:
+#         max_indices = [x + batch_idx * dev_batch_size for x in max_indices_reranked]
+#       else:
+#         max_indices = [x + batch_idx * dev_batch_size for x in max_indices]
 
-      if total_max_indices is None:
-        total_max_indices = max_indices
-      else:
-        total_max_indices += max_indices
+#       if total_max_indices is None:
+#         total_max_indices = max_indices
+#       else:
+#         total_max_indices += max_indices
     
-  print(f'accuracy: {100 * accuracy_score(dev_Y, total_output):.3f}%')
-  print(f'precision: {100 * precision_score(dev_Y, total_output):.3f}%')
-  print(f'recall: {100 * recall_score(dev_Y, total_output):.3f}%')
-  print(f'f1: {100 * f1_score(dev_Y, total_output):.3f}%')
+#   print(f'accuracy: {100 * accuracy_score(dev_Y, total_output):.3f}%')
+#   print(f'precision: {100 * precision_score(dev_Y, total_output):.3f}%')
+#   print(f'recall: {100 * recall_score(dev_Y, total_output):.3f}%')
+#   print(f'f1: {100 * f1_score(dev_Y, total_output):.3f}%')
   
-  cands_dev_df = dev_df.iloc[total_max_indices]
-  cands_dev_df.to_csv(CANDS_PATH, index=False)
+#   cands_dev_df = dev_df.iloc[total_max_indices]
+#   cands_dev_df.to_csv(CANDS_PATH, index=False)
 
-  num_q = int(args.topk * dev_df.shape[0]/dev_batch_size)
-  print(f'expected size {num_q}, actual size {cands_dev_df.shape[0]}')
+#   num_q = int(args.topk * dev_df.shape[0]/dev_batch_size)
+#   print(f'expected size {num_q}, actual size {cands_dev_df.shape[0]}')
 
-  assert(num_q == cands_dev_df.shape[0])
+#   assert(num_q == cands_dev_df.shape[0])
 
-  print(f'#questions is {num_q}, cands shape {cands_dev_df.shape}')
+#   print(f'#questions is {num_q}, cands shape {cands_dev_df.shape}')
 
 if __name__ == '__main__':
   evaluate_picard('dev_picard_join')
