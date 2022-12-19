b069b047bd02f4189fd8737f0355da4114bc7f0e
peterbaile <peterchenwflms@gmail.com>
2022-12-20 07:34:57 +0800

    update

--- BERT/bert-table.sh
+++ BERT/bert-table.sh
@@ -2,9 +2,7 @@
 
 # Loading the required module
 source /etc/profile
-conda deactivate
 module load anaconda/2021b
-conda activate
 
 # Run the script
 # python table_ranking.py --mode train --path train_spider
